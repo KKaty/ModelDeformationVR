feb573a79894831adeeb5960db44a217b139a1b1
YuanjuZHU <eliteyjz@gmail.com>
2020-02-28 16:19:08 +0100

    new unity packege

---
+++ Meshes Cages and Coordinates/testImportFilesFinal/Assets/ReadCoord.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Accord;
+using System.IO;
+using System;
+using Accord.Math;
+
+
+public class ReadCoord : MonoBehaviour
+{
+    //public ReadFile ReadFile;
+    public string fileName;
+    int rowNumber = new int();
+    int columnNumber = new int();
+    StreamReader r;
+    //public struct Matrix { };
+    public double[,] TestMatrix = Matrix.Create(new double[10, 10]);
+
+    void Start()
+    {
+        string path = (Application.streamingAssetsPath + "/" + fileName + ".txt");
+        FileInfo fInfo = new FileInfo(path);
+        if (fInfo.Exists)
+        {
+            r = new StreamReader(path);
+        }
+        else
+        {
+            Debug.Log("File does not exist");       
+        }
+        var fileContent = r.ReadToEnd();
+        var fileLine = fileContent.Split('\n');
+        var matrixDataLine = fileLine.Get(1);
+        //Read the number of rows and columns from the second line of the file
+        string[] matrixData = matrixDataLine.Split(' ');
+        rowNumber = Int32.Parse(matrixData.Get(0));
+        Debug.Log("This is matrix B's row size:" + rowNumber);
+        columnNumber = Int32.Parse(matrixData.Get(1));
+        Debug.Log("This is matrix B's column size:" + columnNumber);
+        //Initialize the Barycentric Matrix(size specification)
+        double[,] Barycentric = Matrix.Create(new double[rowNumber, columnNumber]);
+        //assign the matrix with the data in ".coord" file
+        for (int i = 2; i < rowNumber + 2; i++)
+        {
+            var line = fileLine.Get(i);
+            int k = i - 1;
+            Debug.Log("To see the " + k + "th " + "row:" + line);
+            var eachData = line.Split(' ');
+            for (int j = 0; j < columnNumber; j++)
+            {
+                Barycentric[i - 2, j] = float.Parse(eachData.Get(j));
+                int l = j + 1;
+                Debug.Log("To see [" + k + "," + l + "] element in Matrix B:" + Barycentric[i - 2, j].ToString("F11"));
+            }
+        }
+        Barycentric.Multiply(2);
+        TestMatrix.Multiply(2);
+        //double[] a = ReadFile.MatrixMandG.Multiply(2);
+
+
+    }
+}
---
+++ Meshes Cages and Coordinates/testImportFilesFinal/Assets/ReadFile.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Accord;
+using System.IO;
+using System;
+using Accord.Math;
+using System.Linq;
+
+
+public class ReadFile : MonoBehaviour
+{
+    //public ReadCoord ReadCoord;
+    public string modelFileName;
+    public string cageFileName;
+    public string barCoordFileName;
+
+
+    void Start() 
+    {
+        var modelMatrices = ReadMatrixFromFile(modelFileName, false);
+        Debug.Log("load matrix 1");
+
+        var cageMatrices = ReadMatrixFromFile(cageFileName, false);
+        Debug.Log("load matrix 2");
+
+        var barMatrices = ReadMatrixFromFile(barCoordFileName, true);
+        Debug.Log("load matrix 3");
+    }
+
+
+    public StreamReader readFile(string fileName)
+    {
+        StreamReader r;
+
+        string path = (Application.streamingAssetsPath + "/" + fileName + ".txt");
+        FileInfo fInfo = new FileInfo(path);
+        if (fInfo.Exists)
+        {
+            r = new StreamReader(path);
+        }
+        else
+        {
+            Debug.Log("File does not exist");
+            return null;
+        }
+
+        return r;
+    }
+    public double[,] ReadMatrixFromFile(string fileName, bool isCoordinateFile)
+    {
+        int rowNumber;
+        int columnNumber;
+        int startingRowIndex;
+
+        var r = readFile(fileName);
+        var fileContent = r.ReadToEnd();
+        var fileLine = fileContent.Split('\n');
+       
+        //We set the number of row and column according to the file we are reading.
+        if (isCoordinateFile)
+        {
+            startingRowIndex = 2;
+            var firstFileLine = fileLine.Get(1);
+            //Read the number of rows and columns from the second line of the file
+            string[] matrixData = firstFileLine.Split(' ');
+            rowNumber = Int32.Parse(matrixData.Get(0));
+            Debug.Log("This is matrix B's row size:" + rowNumber);
+            columnNumber = Int32.Parse(matrixData.Get(1));
+            Debug.Log("This is matrix B's column size:" + columnNumber);
+            double[,] Barycentric = Matrix.Create(new double[rowNumber, columnNumber]);
+            for (int i = 2; i < rowNumber + 2; i++)
+            {
+                var line = fileLine.Get(i);
+                int k = i - 1;
+                Debug.Log("To see the " + k + "th " + "row:" + line);
+                var eachData = line.Split(' ');
+                for (int j = 0; j < columnNumber; j++)
+                {
+                    Barycentric[i - 2, j] = float.Parse(eachData.Get(j));
+                    int l = j + 1;
+                    Debug.Log("To see [" + k + "," + l + "] element in Matrix B:" + Barycentric[i - 2, j].ToString("F11"));
+                }
+            }
+            return Barycentric;
+        }
+        else
+        {
+            startingRowIndex = 0;
+            rowNumber = fileLine.Length - 1;
+            columnNumber = 3;
+        }
+
+
+        //Initialize a Matrix (size specification)
+        var MatrixMandG = Matrix.Create(new double[rowNumber, columnNumber]);
+       
+        
+        //assign the matrix with the data in file
+        for (int i = startingRowIndex; i < rowNumber; i++)
+        {
+            var line = fileLine.Get(i);
+            int k = i + 1;
+            Debug.Log("To see the " + k + "th " + "row:" + line);
+           
+            var eachData = line.Split(' ');
+       
+            for (int j = 0; j < columnNumber; j++)
+            {
+                MatrixMandG[i, j] = float.Parse(eachData.Get(j));
+                int l = j + 1;
+                if (fileName.Contains("cage"))
+                { Debug.Log("To see [" + k + "," + l + "] element in Matrix G:" + MatrixMandG[i, j].ToString("F6")); }
+                //else if (fileName.Contains("coord"))
+                //{ }
+                else /*if (fileName.Contains("coord")!=true)*/
+                {
+                  Debug.Log("To see [" + k + "," + l + "] element in Matrix M:" + MatrixMandG[i, j].ToString("F11"));
+                }
+            }
+        }
+
+        return MatrixMandG;
+    }
+
+
+}
